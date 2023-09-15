997c9a0ac38cc3731786a4e030a29bc30fe511eb
Alexander Dyuzhev <dyuzhev@gmail.com>
2023-09-15 18:02:26 +0300

    class MetanormaCollection update, #51

--- src/main/java/org/metanorma/MetanormaCollection.java
+++ src/main/java/org/metanorma/MetanormaCollection.java
@@ -20,8 +20,8 @@ public class MetanormaCollection {
     public MetanormaCollection(List<Map.Entry<String,String>> inputOutputFiles) {
         this.inputOutputFiles = inputOutputFiles;
     }
-    public void generate(String inputFolder) throws IOException {
-        if (inputFolder != null && !inputFolder.isEmpty()) {
+    public void generate(String outputFolder) throws IOException {
+        if (outputFolder != null && !outputFolder.isEmpty()) {
             // Generate metanorma.yml in the root of path
             StringBuilder metanormaYml = new StringBuilder();
             metanormaYml.append("---").append("\n")
@@ -29,7 +29,7 @@ public class MetanormaCollection {
                     .append("  source:").append("\n")
                     .append("    files:").append("\n");
 
-            URI pathRootFolderURI = Paths.get(inputFolder).toUri();
+            URI pathRootFolderURI = Paths.get(outputFolder).toUri();
             for (Map.Entry<String, String> entry : inputOutputFiles) {
                 String resultAdoc = entry.getValue();
                 URI resultAdocURI = Paths.get(resultAdoc).toUri();
@@ -42,7 +42,7 @@ public class MetanormaCollection {
                     .append("    name: " + NAME).append("\n");
 
             //append string buffer/builder to buffered writer
-            BufferedWriter writer = new BufferedWriter(new FileWriter(Paths.get(inputFolder, "metanorma.yml").toFile()));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(Paths.get(outputFolder, "metanorma.yml").toFile()));
             writer.write(metanormaYml.toString());
             writer.close();
         }
